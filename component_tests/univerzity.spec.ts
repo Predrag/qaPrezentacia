518a305937fbf4e1782694ee5bee651aa7cc205c
predrag <martin.durisin@gmail.com>
2024-09-20 08:58:43 +0200

    filtrovanie

--- component_tests/univerzity.spec.ts
+++ component_tests/univerzity.spec.ts
@@ -1,6 +1,24 @@
 import { test, expect } from '@playwright/experimental-ct-vue'
 import TheUniverzity from '../src/components/TheUniverzity.vue'
 
+const univerzity = [
+  {
+    country: 'C1',
+    web_pages: ['W1'],
+    name: 'N1'
+  },
+  {
+    country: 'C2',
+    web_pages: ['W2'],
+    name: 'N2'
+  },
+  {
+    country: 'C3',
+    web_pages: ['W3'],
+    name: 'N3'
+  }
+]
+
 test.use({ viewport: { width: 500, height: 500 } })
 
 test.describe('Otestuj formular na hladanie univerzit', () => {
@@ -27,5 +45,17 @@ test.describe('Otestuj formular na hladanie univerzit', () => {
       return window.getComputedStyle(tl).backgroundColor
     })
     expect(farbaTlacidlo).toBe('rgb(76, 175, 80)')
+  })
+  test('Otestuj, tlacidlo Odoslat', async ({ mount, page }) => {
+    await page.route('**/search?country=slovakia', (route) => {
+      const json = [univerzity][0]
+      route.fulfill({ status: 200, body: JSON.stringify(json) })
+    })
+    const component = await mount(TheUniverzity)
+    const tlacidloOdoslat = await component.getByTestId('formular-btn')
+    const textovePole = await component.getByTestId('formular-input')
+    await textovePole.fill('N1')
+    await tlacidloOdoslat.click()
+    await expect(page.locator('[data-test-id="N1"]')).toHaveText('N1')
   })
 })
